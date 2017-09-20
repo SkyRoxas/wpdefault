ff3ff1092627b0fcc50abb14c37ca7020be09965
skyroxas <skyroxas1521@gmail.com>
2017-09-20 18:15:42 +0800

    add gulp-sass

---
+++ single.php
@@ -0,0 +1,33 @@
+<!--<body>-->
+
+  <!--<header>-->
+  <?php get_header(); ?>
+  <!--</header>-->
+
+  <div id ="content" role="main">
+
+    <!-- Page content -->
+
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <article>
+      <div class="title">
+        <a href="<?php the_permalink();?>">
+          <?php the_title(); ?>
+        </a>
+      </div>
+      <div class="body">
+         <?php the_content(); ?>
+      </div>
+    </article>
+
+    <?php endwhile; else : ?>
+  <?php endif; ?>
+
+  </div>
+
+  <?php get_sidebar(); ?>
+
+  <?php get_footer(); ?>
+
+<!--</body>-->
