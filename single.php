60909b6116a1599c91cc06e537ca18164f1e64f5
skyroxas <skyroxas1521@gmail.com>
2017-09-21 14:08:48 +0800

    add div#page

--- single.php
+++ single.php
@@ -4,29 +4,37 @@
   <?php get_header(); ?>
   <!--</header>-->
 
-  <div id ="content" role="main">
+  <div id="page">
 
-    <!-- Page content -->
+    <div class="wrap">
 
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div id ="content" role="main">
 
-    <article>
-      <div class="title">
-        <a href="<?php the_permalink();?>">
-          <?php the_title(); ?>
-        </a>
+        <!-- Page content -->
+
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <article>
+          <div class="title">
+            <a href="<?php the_permalink();?>">
+              <?php the_title(); ?>
+            </a>
+          </div>
+          <div class="body">
+             <?php the_content(); ?>
+          </div>
+        </article>
+
+        <?php endwhile; else : ?>
+      <?php endif; ?>
+
       </div>
-      <div class="body">
-         <?php the_content(); ?>
-      </div>
-    </article>
 
-    <?php endwhile; else : ?>
-  <?php endif; ?>
+      <?php get_sidebar(); ?>
+
+    </div>
 
   </div>
-
-  <?php get_sidebar(); ?>
 
   <?php get_footer(); ?>
 
